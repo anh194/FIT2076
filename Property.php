d638d47889f3d9db55d27d768945eedc3bd92e92
rvon4 <rvon4@student.monash.edu>
2016-10-05 17:07:32 +1100

    Add files via upload

--- Property.php
+++ Property.php
@@ -7,8 +7,7 @@
 ?>
 <html>
 	<body>		
-		<a href = "PropModify.php?Action=Create">Create New</a> <br/>
-		
+		<input type = "button" value = "New Property" onclick="window.location.href='PropModify.php?Action=Create';"/>
 		<!--Properties Table-->
 		<table border = "1">
 			<tr>
@@ -18,7 +17,6 @@
 				<th>Bathrooms</th>
 				<th>Description</th>
 				<th>Type</th>
-				<th>View</th>
 				<th>Edit</th>
 				<th>Delete</th>
 			</tr>
@@ -61,7 +59,6 @@
 					<td><?php echo $propRow["PROP_DESCRIPTION"]; ?></td>
 					<td><?php echo $propType; ?></td>
 					
-					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=View">View</a></td>
 					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=Update">Edit</a></td>
 					<td><a href = "PropModify.php?propid=<?php echo $propRow["PROP_ID"]?>&Action=Delete">Delete </a></td>
 				</tr>
@@ -71,5 +68,11 @@
 			oci_close($conn);
 		?>
 		</table>
+		<p>	
+			<a href="DisplaySource.php?filename=Property.php">
+				<img src="buttons/property_button.PNG" alt="Property Source Code Button" width = "200px" height = "30px"/>	
+			</a>
+		</p>
+		
 	</body>
 </html>
